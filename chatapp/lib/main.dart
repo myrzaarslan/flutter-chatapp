7391f07a9639fc2b814799cd9f055230904cab63
myrzaarslan <arsssslan@mail.ru>
2025-04-20 19:06:05 +0500

    firebase connected

--- chatapp/lib/main.dart
+++ chatapp/lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:chatapp/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:chatapp/auth/login_or_register.dart';
 import 'package:chatapp/themes/light_mode.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
