08e3958364972cacd9273f83008ab15e17648528
myrzaarslan <arsssslan@mail.ru>
2025-04-20 16:40:13 +0500

    login page added/styled

---
+++ chatapp/lib/pages/login_page.dart
@@ -0,0 +1,78 @@
+import 'package:chatapp/components/my_button.dart';
+import 'package:chatapp/components/my_textfiled.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _pwController = TextEditingController();
+
+  LoginPage({super.key});
+
+  void login() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            Icons.message,
+            size: 60,
+            color: Theme.of(context).colorScheme.primary,
+          ),
+
+          const SizedBox(height: 50),
+
+          Text(
+            "Welcome back!",
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.primary,
+              fontSize: 16,
+            ),
+          ),
+
+          const SizedBox(height: 25),
+
+          MyTextfiled(
+            hintText: "Email",
+            obscureText: false,
+            controller: _emailController,
+          ),
+
+          const SizedBox(height: 10),
+
+          MyTextfiled(
+            hintText: "Password",
+            obscureText: true,
+            controller: _pwController,
+          ),
+
+          const SizedBox(height: 25),
+
+          MyButton(text: "Login", onTap: login),
+
+          const SizedBox(height: 25),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Not a member? ",
+                style: TextStyle(color: Theme.of(context).colorScheme.primary),
+              ),
+              Text(
+                "Register Now",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
